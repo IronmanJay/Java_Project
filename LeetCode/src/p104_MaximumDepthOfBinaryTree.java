5ba91fe09f3aecf66c1806eb7236365d3596a29a
IronmanJay <1975686676@qq.com>
2020-04-13 20:47:02 +0800

    2020.04.13

---
+++ LeetCode/src/p104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+public class p104_MaximumDepthOfBinaryTree {
+    p104_MaximumDepthOfBinaryTree left;
+    p104_MaximumDepthOfBinaryTree right;
+    int val;
+
+    p104_MaximumDepthOfBinaryTree(int val) {
+        this.val = val;
+    }
+
+    //返回二叉树的深度
+    static int getDepth(p104_MaximumDepthOfBinaryTree root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+
+
+    public static void main(String[] args) {
+        p104_MaximumDepthOfBinaryTree root = new p104_MaximumDepthOfBinaryTree(1);
+        p104_MaximumDepthOfBinaryTree left1 = new p104_MaximumDepthOfBinaryTree(9);
+        p104_MaximumDepthOfBinaryTree left2 = new p104_MaximumDepthOfBinaryTree(3);
+        p104_MaximumDepthOfBinaryTree right1 = new p104_MaximumDepthOfBinaryTree(10);
+        //创建一棵树
+        root.left = left1;
+        left1.right = left2;
+        root.right = right1;
+        System.out.println("树的深度是：" + getDepth(root));
+    }
+}
