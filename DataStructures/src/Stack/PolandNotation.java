b475c32926765d4c3b22b1784bb78b57b9e61076
IronmanJay <1975686676@qq.com>
2020-06-16 23:33:46 +0800

    2020.06.14

--- DataStructures/src/Stack/PolandNotation.java
+++ DataStructures/src/Stack/PolandNotation.java
@@ -10,21 +10,21 @@ public class PolandNotation {
 
         // 完成将一个中缀表达式转换成后缀表达式
         // 1+((2+3)*4)-5 -> 1 2 3 + 4 * + 5 -
-        String expression = "1+((2+3)*4)-5";
-        List<String> infixExpressionList = toInfixExpressionList(expression);
-        System.out.println("中缀表达式对应的List" + infixExpressionList);
-        List<String> parseSuffixExpressionList = parseSuffixExpressionList(infixExpressionList);
-        System.out.println("后缀表达式对应的List" + parseSuffixExpressionList);
-        System.out.printf("expression=%d",calculate(parseSuffixExpressionList));
+//        String expression = "1+((2+3)*4)-5";
+//        List<String> infixExpressionList = toInfixExpressionList(expression);
+//        System.out.println("中缀表达式对应的List" + infixExpressionList);
+//        List<String> parseSuffixExpressionList = parseSuffixExpressionList(infixExpressionList);
+//        System.out.println("后缀表达式对应的List" + parseSuffixExpressionList);
+//        System.out.printf("expression=%d",calculate(parseSuffixExpressionList));
 
         // 先定义一个逆波兰表达式
-//        String suffixExpression = "3 4 + 5 * 6 -";
-//        // 1、先将3 4 + 5 * 6 - 放到ArrayList中
-//        // 2、将ArrayList传递给一个方法，遍历ArrayList配合栈完成计算
-//        List<String> rpnList = getListString(suffixExpression);
-//        System.out.println(rpnList);
-//        int res = calculate(rpnList);
-//        System.out.println("计算的结果是：" + res);
+        String suffixExpression = "10 6 9 3 + -11 * / * 17 + 5 +";
+        // 1、先将3 4 + 5 * 6 - 放到ArrayList中
+        // 2、将ArrayList传递给一个方法，遍历ArrayList配合栈完成计算
+        List<String> rpnList = getListString(suffixExpression);
+        System.out.println(rpnList);
+        int res = calculate(rpnList);
+        System.out.println("计算的结果是：" + res);
     }
 
     // 将得到的中缀表达式对应的List -> 后缀表达式对应的List
@@ -104,7 +104,7 @@ public class PolandNotation {
         // 遍历ls
         for (String item : ls) {
             // 这里使用正则表达式来取出数据
-            if (item.matches("\\d+")) { // 匹配的是多位数
+            if (item.matches("^[-\\\\+]?([0-9]+\\\\.?)?[0-9]+$")) { // 匹配的是多位数
                 // 入栈
                 stack.push(item);
             } else {
---
+++ LeetCode/src/Stack/p150_EvaluateReversePolishNotation.java
@@ -0,0 +1,9 @@
+package Stack;
+
+public class p150_EvaluateReversePolishNotation {
+
+    public static void main(String[] args) {
+
+    }
+
+}
